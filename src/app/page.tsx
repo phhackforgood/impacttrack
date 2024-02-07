6ea67f2410464df4cc4cd430cdd6035c5a521098
rxchell <e0969575@u.nus.edu>
2024-02-07 14:45:43 +0800

    Change home page color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-emerald-500">
       <h1 className="text-4xl font-bold text-center">
         ImpactTrack, a new way to track your volunteering impact
       </h1>
