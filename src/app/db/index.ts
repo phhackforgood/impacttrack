f45567b7d0e1a6325f378da1a5c3f0f8b466a1fc
leezhengjing <zhengjing.lee86@gmail.com>
2024-02-07 18:17:06 +0800

    Add database functions
    - Add login function
    - Add register function
    - Add isAuthenticated function
    - Add getUser function

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,4 +1,5 @@
 import PocketBase from 'pocketbase';
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 
 const POCKET_BASE_URL = process.env.POCKET_BASE_URL;
 
@@ -8,6 +9,54 @@ export class DatabaseClient {
     constructor() {
         this.client = new PocketBase(POCKET_BASE_URL);
         this.client.autoCancellation(false);
+    }
+
+    async authenticate(email: string, password: string) {
+        try {
+            const result = await this.client.collection("users").authWithPassword(email, password);
+            console.log('authenticate result:', result);
+            if (!result?.token) {
+                throw new Error("Invalid email or password");
+            }
+            return result;
+        } catch (err) {
+            console.error(err);
+            throw new Error("Invalid email or password");
+        }
+    }
+
+    async register(email: string, password: string) {
+        try {
+            const result = await this.client.collection("users").create({
+                email,
+                password,
+                passwordConfirm: password,
+            });
+
+            return result;
+        } catch (err) {
+
+        }
+    }
+
+    async isAuthenticated(cookieStore: ReadonlyRequestCookies) {
+        const cookie = cookieStore.get('pb_auth');
+        if (!cookie) {
+            return false;
+        }
+
+        this.client.authStore.loadFromCookie(cookie?.value || '');
+        return this.client.authStore.isValid || false
+    }
+
+    async getUser(cookieStore: ReadonlyRequestCookies) {
+        const cookie = cookieStore.get('pb_auth');
+        if (!cookie) {
+            return false;
+        }
+
+        this.client.authStore.loadFromCookie(cookie?.value || '');
+        return this.client.authStore.model;
     }
 
     async getEvents() {
