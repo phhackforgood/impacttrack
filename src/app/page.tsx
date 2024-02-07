27dd8411a0262c5ffe408b26fe55f0d1fdccccdb
leezhengjing <zhengjing.lee86@gmail.com>
2024-02-07 19:06:33 +0800

    Fix EventCard margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col justify-between items-center px-24 py-32 bg-gray-100">
       {events?.map((event, index) => {
         return (
-          <div key={index} className="mb-4">
+          <div key={index} className="mr-auto w-full mb-4">
             <EventCard title={event.title} description={event.description} />
           </div>
         );
