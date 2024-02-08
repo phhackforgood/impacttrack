6dbd7c8bbcc87f01145438734a174097b9881b8f
rxchell <e0969575@u.nus.edu>
2024-02-08 13:17:56 +0800

    Added buttons and dropdown. BUT how do you SINGLE SELECT the buttons

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -36,25 +36,53 @@ function FormPage() {
             setError('Failed to submit form 2'); 
         } 
     }; 
- 
-    return ( 
-        <div className="flex flex-col items-center">
-            <h1 className="text-2xl font-medium mx-auto items-center font-Dmsans mt-3" >Volunteer Form</h1>
-            
+    
+    return (
+        <div className="flex flex-col justify-end items-center">
+            <h1 className="text-4xl font-medium mx-auto items-center font-Dmsans mt-5" >Volunteer Form</h1>
+
             <section className="mt-10">
+
             <form className="flex flex-col" method="POST" action="#">
-                    <label className="block text-gray-700 text-sm font-normal font-Dmsans mb-2 ml-3" >Write Up</label>
-                <div className="mb-6 pt-3 rounded bg-gray-100">
-                    <input type="string" className="bg-white rounded w-full text-gray-700 border-b-4 outline-gray-300 focus:outline-gray-400 transition duration-500 px-3 pb-3"/>
+
+                <label className=" block text-gray-700 text-sm font-semibold" >Your profile</label>
+                <div className="mb-8 pt-3 rounded bg-gray-100">
+                    <label>
+                        <input type="radio" className="form-radio"/>
+                        <span className="ml-2 text-gray-700 text-sm">Organiser</span>
+                    </label>
+                    <div></div>
+                    <label>
+                        <input type="radio" className="form-radio"/>
+                        <span className="ml-2 text-gray-700 text-sm">Volunteer</span>
+                    </label>
+                    <div></div>
+                    <label>
+                        <input type="radio" className="form-radio"/>
+                        <span className="ml-2 text-gray-700 text-sm">Beneficiary</span>
+                    </label>
                 </div>
-                    <label className="block text-gray-700 text-sm font-normal mb-2 ml-3" >Hours</label>
-                <div className="mb-6 pt-3 rounded bg-gray-100">
-                    <input type="integer" className="bg-white rounded w-full text-gray-700 border-b-4 outline-gray-300 focus:outline-gray-400 transition duration-500 px-3 pb-3"/>
+
+                <label className="block text-gray-700 text-sm font-semibold" >Event</label>
+                <div className="mb-8 pt-3 rounded bg-gray-100">
+                    <select className="pt-3 bg-white rounded w-full text-gray-700 text-sm border-2 outline-gray-300 focus:outline-gray-400 transition duration-500 px-3 pb-3">
+                        <option value="Ongoing Events">Ongoing Events</option>
+                        <option value="Earth Oven">Earth Oven</option>
+                        <option value="Wood Upcycling">Wood Upcycling</option>
+                        <option value="Glass Upcycling">Glass Upcycling</option>
+                    </select>
                 </div>
-                    <label className="block text-gray-700 text-sm font-normal mb-2 ml-3" >Event</label>
-                <div className="mb-6 pt-3 rounded bg-gray-100">
-                    <input type="string" className="bg-white rounded w-full text-gray-700 border-b-4 outline-gray-300 focus:outline-gray-400 transition duration-500 px-3 pb-3"/>
+
+                <label className="block text-gray-700 text-sm font-semibold" >Hours</label>
+                <div className="mb-8 pt-3 rounded bg-gray-100">
+                    <input type="integer" className=" pt-3 bg-white rounded w-full text-gray-700 border-2 outline-gray-300 focus:outline-gray-400 transition duration-500 px-3 pb-3"/>
                 </div>
+
+                <label className="block text-gray-700 text-sm font-semibold font-Dmsans" >Write Up</label>
+                <div className="mb-8 pt-3 rounded bg-gray-100">
+                    <input type="string" className="pt-3 bg-white rounded w-full text-gray-700 border-2 outline-gray-300 focus:outline-gray-400 transition duration-500 px-3 pb-3"/>
+                </div>
+
                 <div className="flex justify-end">
                     <a href="#" className="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">Back</a>
                 </div>
@@ -62,8 +90,7 @@ function FormPage() {
             </form>
         </section>
         </div>
-    ) 
-} 
- 
-export default FormPage
+    )
+}
 
+export default FormPage;
