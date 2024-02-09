dff94d7bcb58e9cd7e7222e7c2a41df7d522ea92
Lee Zheng Jing <zhengjing.lee86@gmail.com>
2024-02-10 01:39:50 +0800

    Create profile fetch

--- src/app/components/EventCard.tsx
+++ src/app/components/EventCard.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import ProgressBar from 'react-bootstrap/ProgressBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface EventCardProps {
     title: string;
