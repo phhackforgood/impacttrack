dd7b42b7293cbefe72e3121ec398e8475555077c
tahnya <111273056+tahnya@users.noreply.github.com>
2024-02-09 22:29:53 +0800

    Update progressbar

--- src/app/components/EventCard.tsx
+++ src/app/components/EventCard.tsx
@@ -13,7 +13,7 @@ const EventCard = (props: EventCardProps) => {
         <Link href="/events" className="flex flex-col items-center bg-white hover:shadow-xl font-Dmsans w-3/5 h-40 rounded-xl mr-auto">
             <h2 className="text-2xl font-semibold mr-auto mb-3 mx-8 mt-6">{title}</h2>
             <h3 className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full">{description}</h3>
-            <div className="w-full">
+            <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full">
                 <ProgressBar now={progress} label={`${progress}%`} />
             </div>
         </Link>
