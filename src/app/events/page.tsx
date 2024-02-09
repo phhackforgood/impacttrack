c82d1f212a9ed22ed670532075662db212307bc8
tahnya <111273056+tahnya@users.noreply.github.com>
2024-02-09 22:58:40 +0800

    Update events

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,17 +2,28 @@ import Link from "next/link";
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export default function EventPage() {
+    const progVolunteers = 84;
+    const progPhotos = 50;
+    const progSmiles = 57;
+    const max = 250;
+    
     return (
         <main>
-            <div className="flex flex-col items-center bg-white font-Dmsans w-3/5 h-40 rounded-xl mr-auto">
+            <div className="flex flex-col items-center bg-white font-Dmsans hover:shadow-xl w-3/5 h-300 rounded-xl mr-auto">
                 <h1 className="text-2xl font-semibold mr-auto mb-3 mx-8 mt-6">Key Performance Indicators</h1>
-                
-                <h2 className="text-2xl font-semibold mr-auto mb-3 mx-8 mt-6">Volunteers</h2>
-                <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full"><ProgressBar now={50} label={`${50}`} /></div>
-                <h2 className="text-2xl font-semibold mr-auto mb-3 mx-8 mt-6">Photos Uploaded</h2>
-                <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full"><ProgressBar now={50} label={`${50}`} /></div>
-                <h2 className="text-2xl font-semibold mr-auto mb-3 mx-8 mt-6">Smiles Recorded</h2>
-                <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full"><ProgressBar now={50} label={`${50}`} /></div>
+                <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full">
+                    <ProgressBar>
+                        <ProgressBar variant="success" max={max} now={progVolunteers} key={1} />
+                        <ProgressBar variant="warning" max={max} now={progPhotos} key={2} />
+                        <ProgressBar variant="danger" max={max} now={progSmiles} key={3} />
+                    </ProgressBar>
+                </div>
+                <h2 className="text-xl font-semibold mr-auto mb-3 mx-8 mt-6">Volunteers</h2>
+                <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full"><ProgressBar variant="success" now={progVolunteers} label={`${progVolunteers}`} /></div>
+                <h2 className="text-xl font-semibold mr-auto mb-3 mx-8 mt-6">Photos Uploaded</h2>
+                <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full"><ProgressBar variant="warning" now={progPhotos} label={`${progPhotos}`} /></div>
+                <h2 className="text-xl font-semibold mr-auto mb-3 mx-8 mt-6">Smiles Recorded</h2>
+                <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full"><ProgressBar variant="danger" now={progSmiles} label={`${progSmiles}`} /></div>
             </div>
         </main>
     )
