0bcfb276d37664fcdb1c37d460d99daa1eb1d2b6
tahnya <111273056+tahnya@users.noreply.github.com>
2024-02-09 23:11:16 +0800

    Update events

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -8,8 +8,8 @@ export default function EventPage() {
     const max = 250;
     
     return (
-        <main className="flex min-h-screen flex-col justify-between items-center px-24 py-10 bg-gray-100">
-            <div className="flex flex-col items-center bg-white font-Dmsans hover:shadow-xl w-3/5 h-300 rounded-xl mr-auto">
+        <main className="flex min-h-screen flex-col items-center px-24 py-10 bg-gray-300">
+            <div className="flex flex-col items-center font-Dmsans w-3/5 mr-auto mb-3 mt-6">
                 <h1 className="text-2xl font-semibold mr-auto mb-3 mx-8 mt-6">Key Performance Indicators</h1>
                 <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full">
                     <ProgressBar>
@@ -17,7 +17,9 @@ export default function EventPage() {
                         <ProgressBar variant="warning" max={max} now={progPhotos} key={2} />
                         <ProgressBar variant="danger" max={max} now={progSmiles} key={3} />
                     </ProgressBar>
-                </div>
+                </div>    
+            </div>
+            <div className="flex flex-col items-center bg-white font-Dmsans w-3/5 rounded-xl mr-auto mt-100">
                 <h2 className="text-xl font-semibold mr-auto mb-3 mx-8 mt-6">Volunteers</h2>
                 <div className="text-sm font-light text-left text-gray-400 leading-2 text-wrap px-8 w-full"><ProgressBar variant="success" now={progVolunteers} label={`${progVolunteers}`} /></div>
                 <h2 className="text-xl font-semibold mr-auto mb-3 mx-8 mt-6">Photos Uploaded</h2>
